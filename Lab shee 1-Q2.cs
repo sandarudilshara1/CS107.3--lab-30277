02494d16adb07cc48f4733ad08cfea44615bb19b
sandarudilshara1 <157581389+sandarudilshara1@users.noreply.github.com>
2024-02-05 17:56:31 -1200

    Add files via upload

---
+++ Lab shee 1-Q2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_Q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            for (int i=0;j<10;i++)
+            {
+                Console.WriteLine("Enter the number:");
+                int number = int.Parse(Console.ReadLine());
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("it's even");
+                }
+                else
+                {
+                    Console.WriteLine("it's odd");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
